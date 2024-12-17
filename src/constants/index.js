e91c0e2b321f7fcda9666c70c93e2330c5a325ba
Anorme <anormeinkumsah@gmail.com>
2024-12-17 09:12:57 +0000

    Created first version of portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Hi I am Anorme, the fullstack developer you need. `;
+
+export const ABOUT_TEXT = `My goal is to grow by creating innovative and impactful solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const PROJECTS = [
+  {
+    title: "Smart-brain",
+    image: project1,
+    description:
+      "A fully functional fullstack website that enables users to detect faces in images using AI.",
+    technologies: ["HTML", "CSS","JavaScript", "React", "Node.js", "Express.js","PostgreSQL"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "JavaScript","TailwindCSS","Framer Motion"],
+  }
+];
+
+export const CONTACT = {
+  address: "X378, World Vision St, Dodowa",
+  phoneNo: "+233 5978 429 21 ",
+  email: "anormeinkumsah@gmail.com",
+};

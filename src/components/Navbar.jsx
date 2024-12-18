dbaa26ffe6b9340e572405cf32944908e13be1ac
Anorme <anormeinkumsah@gmail.com>
2024-12-18 23:52:35 +0000

    tweaked navbar and package.json

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import React, {useState, useEffect} from 'react';
 
 const Navbar = () => {
   return (
-      <div className="fixed z-40 top-0 left-0 right-0 bg-neutral-950 border-b border-neutral-700">
+      <div className="fixed z-40 top-0 left-0 right-0 bg-neutral-950 border-b border-neutral-700 ">
         <nav className="container flex items-center justify-between py-1 lg:py-5">
           <div className="flex flex-shrink-0 items-center w-1/2">
             <img className="mx-2 w-20 cursor-pointer" src={logo} alt="logo"/>

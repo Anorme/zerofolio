566c64a521bc57f9ffa4c4e3623595f5a6932b4d
Anorme <anormeinkumsah@gmail.com>
2024-12-18 21:46:41 +0000

    improved navbar and hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import logo from '../assets/AILogo.png';
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
-import { FaInstagram } from "react-icons/fa";
+import {routes} from "../routes";
+import NavDesktop from "./NavDesktop";
+import NavMobile from "./NavMobile";
+import { Link } from "react-scroll";
+import React, {useState, useEffect} from 'react';
+
+
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-20" src={logo} alt="logo"/>
+      <div className="fixed z-40 top-0 left-0 right-0 bg-neutral-950 border-b border-neutral-700">
+        <nav className="container flex items-center justify-between py-1 lg:py-5">
+          <div className="flex flex-shrink-0 items-center w-1/2">
+            <img className="mx-2 w-20 cursor-pointer" src={logo} alt="logo"/>
+          </div>
+          <NavMobile />
+          <NavDesktop />
+        </nav>
       </div>
-      <div className='m-8 flex items-center justify-center gap-4'>
-        <FaLinkedin />
-        <FaGithub /> 
-        <SiGmail />
-        <FaInstagram />
-      </div>
-    </nav>
   )
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,10 +16,10 @@ const Projects = () => {
         <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
             <div className="w-full max-w-xl lg:w-1/2 lg:pt-10">
               <h6 className="mb-2 font-semibold" > {project.title}</h6>
-              <p className="mb-4 text-neutral-400">{project.description}</p>
+              <p className="mb-4 text-neutral-400 text-2xl">{project.description}</p>
             <div className="flex flex-wrap ml-0 my-8">
               {project.technologies.map((tech, index) => (
-                <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-600">{tech}</span>
+                <span key={index} className="mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-600">{tech}</span>
               ))}
             </div>
             

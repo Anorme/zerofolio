f32e977f8e102ab5e8ae682e91599fb194285963
Anorme <anormeinkumsah@gmail.com>
2024-12-19 12:37:38 +0000

    changed footer to render from an array

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,22 @@ import { CONTACT } from "../constants";
 
 const Footer = () => {
   return (
-    <div className="border-b border-neutral-900 lg:pt-20 ">
-    <div className="text-center tracking-tighter flex flex-col lg:flex-row place-content-center align-middle justify-between">
-      <p className="my-4 ">{CONTACT.address}</p>
-      <p className="my-4">{CONTACT.phoneNo}</p>
-      <p className="my-4 no-underline ">{CONTACT.email}</p>
-      <p className="my-4">AnormeInkumsah2024</p>
-    </div>
+    <div className="hidden border-b border-neutral-900 lg:pt-20 lg:flex justify-between">
+      {CONTACT.map((contact,i) => {
+        const {address, phoneNo, email, name, Icon} = contact;
+        return (
+          <div key={i} className="text-center tracking-tighter flex flex-col lg:flex-row place-content-center align-middle justify-between">
+            <p className="flex p-2 place-content-center items-center">
+              {address}
+              {phoneNo}
+              {email}
+              {name}
+              <Icon className="ml-2"/>
+            </p>
+        </div>
+        )
+      })}
+
     
   </div>
   )

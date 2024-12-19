188ce07170fe29f97caa7dfbfd4874caa7bc9f85
Anorme <anormeinkumsah@gmail.com>
2024-12-19 11:30:18 +0000

    Added padding top to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { CONTACT } from "../constants";
 
 const Footer = () => {
   return (
-    <div className="border-b border-neutral-900 pb-20 ">
+    <div className="border-b border-neutral-900 pt-20 ">
     <div className="text-center tracking-tighter flex flex-col lg:flex-row place-content-center align-middle justify-between">
       <p className="my-4 ">{CONTACT.address}</p>
       <p className="my-4">{CONTACT.phoneNo}</p>

9a7cd253125c19e96d143c20c6d79f3e1a0891f9
Anorme <anormeinkumsah@gmail.com>
2024-12-20 19:41:48 +0100

    removed container class from nav in Navbar and added padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ import NavMobile from "./NavMobile";
 const Navbar = () => {
   return (
       <div className="fixed z-40 top-0 left-0 right-0 bg-neutral-950 border-b border-neutral-700 ">
-        <nav className="container flex items-center justify-between py-1 lg:py-5">
+        <nav className=" w-full flex items-center justify-between px-1 py-1 lg:py-5">
           <div className="flex flex-shrink-0 items-center w-1/2">
-            <img className="mx-2 w-20 cursor-pointer" src={logo} alt="logo"/>
+            <img className=" w-20 cursor-pointer" src={logo} alt="logo"/>
           </div>
           <NavMobile />
           <NavDesktop />

05de3ec5542f82ce869dd0cbec8810c231ff5f2c
Anorme <anormeinkumsah@gmail.com>
2024-12-21 11:23:26 +0100

    tweaked spacing between project section elements

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,11 +16,11 @@ const Projects = () => {
       </motion.h2>
 
       <div>{PROJECTS.map((project, index) => (
-        <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+        <div key={index} className="mb-20 flex flex-wrap lg:justify-center">
             <div className="w-full max-w-xl lg:w-1/2 lg:pt-10">
               <h6 className="mb-2 font-semibold" > {project.title}</h6>
               <p className="mb-4 text-neutral-400 text-2xl">{project.description}</p>
-            <div className="flex flex-wrap ml-0 my-8">
+            <div className="flex flex-wrap ml-0 my-2">
               {project.technologies.map((tech, index) => (
                 <span key={index} className="mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-600">{tech}</span>
               ))}

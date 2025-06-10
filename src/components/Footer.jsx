a5065f1e35caeed01e6ad7803527992b9ab227b1
Anorme <anormeinkumsah@gmail.com>
2025-06-10 20:43:19 +0000

    Conditionally render footer view based on device type

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,17 @@
 import FooterDesktop from "./FooterDesktop";
 import FooterMobile from "./FooterMobile";
+import useDeviceType from "./useDeviceType";
 
 const Footer = () => {
+  const deviceType = useDeviceType()
+
   return (
     <div>
-      <FooterMobile />
-      <FooterDesktop />
+      {
+        deviceType === 'desktop' ? <FooterDesktop /> : <FooterMobile />
+      }      
     </div>
-   
-  )
-}
+  );
+};
 
 export default Footer

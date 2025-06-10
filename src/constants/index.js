ba8d96b83e95ab8774e2b02a620f33c6070aa5ed
Anorme <anormeinkumsah@gmail.com>
2025-06-10 20:45:02 +0000

    Model new data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,7 @@
 import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-2.png";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
-import { FaLocationDot } from "react-icons/fa6";
-import { FaPhone } from "react-icons/fa";
-import { FaCopyright } from "react-icons/fa";
+
+import { FaCopyright, FaLinkedin, FaGithub } from "react-icons/fa";
 import { CiMail } from "react-icons/ci";
 
 export const HERO_CONTENT = `I am the fullstack developer you need.`;
@@ -23,36 +20,39 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional fullstack website that enables users to detect faces in images using AI.",
-    technologies: ["HTML", "CSS","JavaScript", "React", "Node.js", "Express.js","PostgreSQL"],
+    technologies: ["HTML", "CSS","JavaScript", "React.js", "Node.js", "Express.js","PostgreSQL"],
     link: "https://github.com/Anorme/smartbrain-frontend",
     live: "https://smartbrain-frontend-eight.vercel.app/",
   },
   {
-    title: "Portfolio Website",
+    title: "Space Mission Scheduling API",
     image: project2,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "JavaScript","TailwindCSS","Framer Motion"],
-    link:"https://github.com/Anorme/coolfolio",
-    live:"https://zerofolio.vercel.app/"
+      "Integrated MongoDB to manage mission data, ensuring seamless updates and rapid retrieval. ",
+    technologies: [ "JavaScript", "React.js", "Node.js", "Express.js", "MongoDB", "Docker", "AWS EC2"],
+    link:"https://github.com/Anorme/nasa-project",
+    live:"http://54.174.44.182:8000/launch"
   }
 ];
 
 export const CONTACT = [
   {
-    address: "X378, World Vision St, Dodowa",
-    Icon: FaLocationDot  ,
-  },
-  {
-    phoneNo: "+233 5978 429 21 ",
-    Icon: FaPhone, 
-  },
-  {
-    email: "anormeinkumsah@gmail.com",
-    Icon: CiMail ,
-  },
-  {
-    name: "AnormeInkumsah",
+    name: "2025 Anorme Inkumsah. All rights reserved",
     Icon: FaCopyright, 
-  }
+  },
+];
+
+export const LINKS = [
+  {
+    Icon: CiMail,
+    link: "mailto:anormeinkumsah@gmail.com",
+  },
+  {
+    Icon: FaLinkedin,
+    link: "https://www.linkedin.com/in/anormeinkumsah/",
+  },
+  {
+    Icon: FaGithub,
+    link: "https://github.com/Anorme",
+  },
 ];

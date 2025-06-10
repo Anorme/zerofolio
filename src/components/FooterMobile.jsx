2b6b75c26f7ccd97eb97f18397b461f45f2f6ac7
Anorme <anormeinkumsah@gmail.com>
2025-06-10 20:43:45 +0000

    Refactor footer mobile view

--- src/components/FooterMobile.jsx
+++ src/components/FooterMobile.jsx
@@ -1,23 +1,39 @@
 import { CONTACT } from "../constants";
+import { LINKS } from "../constants";
+
 const FooterMobile = () => {
   return (
-    <div className="border-b border-neutral-900 flex flex-col lg:hidden">
-    {CONTACT.map((contact,i) => {
-      const {address, phoneNo, email, name, Icon} = contact;
-      return (
-        <div key={i} className="text-center tracking-tighter flex flex-col lg:flex-row place-content-center align-middle justify-between">
-          <p className="flex p-2 place-content-center items-center">
-            {address}
-            {phoneNo}
-            {email}
-            {name}
-            <Icon className="ml-2"/>
-          </p>
-      </div>
-      )
-    })}
-  </div>
-  )
+    <div className="border-b border-neutral-900 flex flex-col lg:hidden p-4">
+      {CONTACT.map((contact, i) => {
+        const { name, Icon } = contact;
+        return (
+          <div key={i} className="flex items-center justify-between text-center tracking-tighter mb-2">
+            <div className="flex items-center">
+              <Icon className="mr-2" />
+              <span>{name}</span>
+            </div>
+            {/* Social Media Icons */}
+            <div className="flex space-x-4">
+              {LINKS.map((linkInfo, j) => {
+                const { Icon: LinkIcon, link } = linkInfo;
+                return (
+                  <a
+                    key={j}
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500 hover:underline"
+                  >
+                    <LinkIcon className="h-6 w-6" /> 
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default FooterMobile
+export default FooterMobile;

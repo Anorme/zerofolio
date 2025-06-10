33f98abc457463c132e75abfb1dc4a2d8724fd2c
Anorme <anormeinkumsah@gmail.com>
2025-06-10 17:18:35 +0000

    Conditionally render mobile or desktop component depending on view and Keep track of active section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,18 @@
-import logo from '../assets/AILogo.png';
 import NavDesktop from "./NavDesktop";
 import NavMobile from "./NavMobile";
 
-const Navbar = () => {
+import useDeviceType from "./useDeviceType";
+
+const Navbar = ({activeSession}) => {
+  const deviceType = useDeviceType();
+
   return (
-      <div className="fixed z-40 top-0 left-0 right-0 bg-neutral-950 border-b border-neutral-700 ">
-        <nav className=" w-full flex items-center justify-between px-1 py-1 lg:py-5">
-          <div className="flex flex-shrink-0 items-center w-1/2">
-            <img className=" w-20 cursor-pointer" src={logo} alt="logo"/>
-          </div>
-          <NavMobile />
-          <NavDesktop />
-        </nav>
-      </div>
-  )
-}
+    <>
+      {
+        deviceType === 'desktop' ? <NavDesktop activeSession={activeSession} /> : <NavMobile />
+      }      
+    </>
+  );
+};
 
 export default Navbar

02682fb8dd0e403aa5507185fcfda0750ff80fda
Anorme <anormeinkumsah@gmail.com>
2025-06-10 18:40:04 +0000

    Reduce spacing

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -22,12 +22,12 @@ const Technologies = () => {
   const deviceType = useDeviceType();
   
   return (
-    <div className="border-b border-neutral-800 pb-24">
+    <div className="border-b border-neutral-800 pb-12">
       <motion.h1
         whileInView={{opacity: 1, y: 0}}
         initial={{opacity: 0, y: -100}}
         transition={{duration: 1.5}}   
-        className="my-20 text-center text-4xl"
+        className="my-10 text-center text-4xl"
       >
         Technologies
       </motion.h1>

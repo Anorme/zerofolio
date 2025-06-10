02009b80437eccfbb6b5d0d020edd7c5c34e12e8
Anorme <anormeinkumsah@gmail.com>
2025-06-10 17:23:00 +0000

    Remove icons and some styles

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -12,53 +12,59 @@ const NavMobile = () => {
   useClickAway(ref, () => setOpen(false));
 
   return (
-    <div ref={ref} className="lg:hidden mr-2">
-      <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div 
-          initial={{ opacity: 0}}
-          animate={{opacity: 1}}
-          exit={{ opacity: 0}}
-          transition={{ duration: 0.2}}
-          className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20">
-            <ul className="grid gap-2">
-              {routes.map((route, idx) => {
-                const { Icon, title, id } = route;
+    <div className="fixed z-40 top-0 left-0 right-0 bg-neutral-950 border-none ">
+      <nav className=" w-full flex items-center justify-between px-1 py-1 lg:py-5">
+        <div className="flex flex-shrink-0 items-center w-1/2">
+          <span>Anorme Inkumsah</span>
+        </div>
+        <div ref={ref} className="lg:hidden mr-2">
+          <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div 
+              initial={{ opacity: 0}}
+              animate={{opacity: 1}}
+              exit={{ opacity: 0}}
+              transition={{ duration: 0.2}}
+              className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-0 pt-0 bg-neutral-950 border-none">
+                <ul className="grid gap-2">
+                  {routes.map((route, idx) => {
+                    const { title, id } = route;
 
-                return (
-                  <motion.li
-                    initial={{ scale: 0, opacity: 0 }}
-                    animate={{ scale: 1, opacity: 1}}
-                    transition={{ 
-                      type: "spring",
-                      stiffness: 260,
-                      damping: 20,
-                      delay: 0.1 + idx / 10,
-                    }}
-                    key={title}
-                    className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700 "
-                  >
-                    <Link 
-                      to={id}
-                      smooth={true} 
-                      duration={500} 
-                      onClick={() => setOpen((prev) => !prev)}
-                      className={
-                        "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
-                      }
-                      href={id}
-                    >
-                      <span className="flex gap-1 text-lg">{title}</span>
-                      <Icon className="text-xl" />
-                    </Link>
-                  </motion.li>
-                );
-              })}
-            </ul>
-          </motion.div>
-        )}
-      </AnimatePresence>
+                    return (
+                      <motion.li
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1}}
+                        transition={{ 
+                          type: "tween",
+                          stiffness: 260,
+                          damping: 20,
+                          delay: 0.1 + idx / 10,
+                        }}
+                        key={title}
+                        className="w-full p-[0.08rem]  "
+                      >
+                        <Link 
+                          to={id}
+                          smooth={true} 
+                          duration={500} 
+                          onClick={() => setOpen((prev) => !prev)}
+                          className={
+                            "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
+                          }
+                          href={id}
+                        >
+                          <span className="flex gap-1 text-lg">{title}</span>
+                        </Link>
+                      </motion.li>
+                    );
+                  })}
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      </nav>
     </div>
   )
 }

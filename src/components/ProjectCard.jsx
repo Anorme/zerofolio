afbe10cc610da2dcae3e829b9129dbb6f019352d
Anorme <anormeinkumsah@gmail.com>
2025-09-18 20:21:05 +0000

    style(ProjectCard):  switch project card title margin with padding for cleaner layout

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,10 +3,10 @@ import { FaGithub } from "react-icons/fa";
 
 const ProjectCard = ({ title, description, imageUrl, liveLink, repoLink }) => {
   return (
-    <div className="max-w-sm border border-none rounded-lg overflow-hidden shadow-lg m-4">
+    <div className="max-w-sm border border-none rounded-none overflow-hidden shadow-lg m-4">
       <img src={imageUrl} alt={title} className="w-full h-48 object-cover transition-transform transform hover:scale-105" />
       <div className="bg-gray-800 min-h-[150px] md:min-h-[200px]"> {/* Darker background for the card */}
-        <h2 className="text-lg md:text-xl font-bold my-2 text-white text-center">{title}</h2>
+        <h2 className="text-lg md:text-xl font-bold py-2 text-white text-center">{title}</h2>
         <p className="text-sm md:text-lg text-gray-300 text-center mb-4 flex-grow">{description}</p>
         <div className="flex justify-center space-x-4 items-center pb-4">
           <a

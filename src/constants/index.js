e363328b9cfcb0562e8108f515a342516d869e2f
Anorme <anormeinkumsah@gmail.com>
2025-09-19 00:35:06 +0000

    feat(Projects): update project details and descriptions

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-2.png";
-import project3 from "../assets/projects/project-4.png";
+import project3 from "../assets/projects/project-3.png";
 
 import { FaCopyright, FaLinkedin, FaGithub } from "react-icons/fa";
 import { CiMail } from "react-icons/ci";
@@ -8,41 +8,43 @@ import { CiMail } from "react-icons/ci";
 export const HERO_CONTENT = `I am the fullstack developer you need.`;
 
 export const ABOUT_TEXT = [
-  "I am a passionate full stack developer with a knack for crafting robust and scalable web applications.",
-  "With hands-on experience building fullstack projects like Smart-brain, I have honed my skills in front-end technologies like React, as well as back-end technologies like Node.js, Express.js and PostgreSQL.",
+  "You’ve got a vision, maybe a startup, a product, or a platform but you need someone who can turn that into reality. Not just with code, but with leadership, strategy, and relentless execution.",
+  "With hands-on experience leading a cross-functional team building fullstack projects like the misqabbi e-commerce store, I have honed my skills in the MERN stack and proven my capacity for leadership, strategy, and execution.",
   "I consistently leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.",
-  "My goal now is to grow by taking on bigger challenges that are impactful."
+  "If you know what you want and when I’ll handle the how."
 ];
 
 
 export const PROJECTS = [
   {
-    title: "Smart-brain",
+    title: "Nyxtern landing page",
     image: project1,
     description:
-      "A fully functional fullstack website that enables users to detect faces in images using AI.",
-    technologies: ["HTML", "CSS","JavaScript", "React.js", "Node.js", "Express.js","PostgreSQL"],
-    link: "https://github.com/Anorme/smartbrain-frontend",
-    live: "https://smartbrain-frontend-eight.vercel.app/",
+      "Landing page for job marketplace platform",
+    technologies: ["TypeScript", "Next.js", "TailwindCSS", "Shadcn UI"],
+    live: "https://nyxtern-landing.vercel.app/",
+    status: "completed"
+  },
+  {
+    title: "Misqabbi E-commerce Store",
+    image: project3,
+    description:
+      "E-commerce store for Misqabbi, a clothing brand. ",
+    technologies: ["React.js", "TailwindCSS", "Context API"],
+    link:"https://github.com/Anorme/misqabbi-frontend",
+    live:"https://misqabbigh.netlify.app/",
+    status: "in-progress"
   },
   {
     title: "Space Mission Scheduling API",
     image: project2,
     description:
       "Integrated MongoDB to manage mission data, ensuring seamless updates and rapid retrieval. ",
-    technologies: [ "JavaScript", "React.js", "Node.js", "Express.js", "MongoDB", "Docker", "AWS EC2"],
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "Docker", "AWS EC2"],
     link:"https://github.com/Anorme/nasa-project",
-    live:"http://54.174.44.182:8000/launch"
+    status: "completed"
   },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "Portfolio displaying projects. ",
-    technologies: [ "JavaScript", "React.js", "TailwindCSS", "Framer-motion"],
-    link:"https://github.com/Anorme/zerofolio",
-    live:"http://anormeinkumsah.com"
-  },
+  
 ];
 
 export const CONTACT = [
